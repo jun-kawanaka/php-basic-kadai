28a56522b4af06a89f0f996b3f881427d411b0e9
川中潤 <157383282+jun-kawanaka@users.noreply.github.com>
2024-04-07 22:42:13 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>完了</title>
+</head>
+<body>
+  <h1>登録が完了しました。</h1>
+  <p>社員情報がデータベースに保存されました。</p>
+
+  <button onclick="location.href='form.php';">戻る</button>
+</body>
+</html>
